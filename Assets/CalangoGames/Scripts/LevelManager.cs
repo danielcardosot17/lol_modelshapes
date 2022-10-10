bcab5b1d67356a0ac34591cc248080ef0585ca7b
Daniel <danielcardosot17@gmail.com>
2022-10-10 00:24:42 -0300

    Trying to get less than 30mb build

--- Assets/CalangoGames/Scripts/LevelManager.cs
+++ Assets/CalangoGames/Scripts/LevelManager.cs
@@ -1,9 +1,7 @@
-using log4net.Core;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
